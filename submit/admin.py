0532cd86bfb4909aa80754297eaea98b255bc5f0
Zoltan Onody <zoli@ksp.sk>
2017-01-28 15:10:36 +0100

    Improve admin experience (#21)
    * Improve admin experience
    
    * Show timestamp and filter by message in submit admin

--- submit/admin.py
+++ submit/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from submit.models import Submit
 
 
-admin.site.register(Submit)
+class SubmitAdmin(admin.ModelAdmin):
+    list_display = ['user', 'task', 'message', 'language', 'timestamp']
+    search_fields = ['user__username', 'task__id', 'task__task_set__id']
+    list_filter = ['language', 'message']
+
+admin.site.register(Submit, SubmitAdmin)
--- tasks/admin.py
+++ tasks/admin.py
@@ -2,7 +2,33 @@ from django.contrib import admin
 from tasks.models import Task, TaskSet, Stalker, Active
 
 
-admin.site.register(Task)
-admin.site.register(TaskSet)
-admin.site.register(Active)
-admin.site.register(Stalker)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'task_set', 'public', 'type']
+    search_fields = ['id', 'title', 'text', 'example_solution']
+    list_filter = ['type', 'public', 'task_set']
+
+admin.site.register(Task, TaskAdmin)
+
+
+class TaskSetAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'public']
+    search_fields = ['id', 'title']
+    list_filter = ['public']
+
+admin.site.register(TaskSet, TaskSetAdmin)
+
+
+class ActiveAdmin(admin.ModelAdmin):
+    list_display = ['user', 'task', 'task_set']
+    search_fields = ['user__username', 'task__id', 'task_set__id']
+    list_filter = ['task_set']
+
+admin.site.register(Active, ActiveAdmin)
+
+
+class StalkerAdmin(admin.ModelAdmin):
+    list_display = ['task', 'user', 'seen']
+    search_fields = ['task__id', 'task__task_set__id', 'user__username']
+    list_filter = ['task__task_set']
+
+admin.site.register(Stalker, StalkerAdmin)
