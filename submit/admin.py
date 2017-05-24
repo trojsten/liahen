e196360b8733d2cbd9da4a4d7ddaaa446a7f63f4
Zoltan Onody <zoltan.onody@gmail.com>
2017-05-24 18:32:24 +0200

    Add easy-select-2 to admin (#25)

--- submit/admin.py
+++ submit/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from easy_select2 import select2_modelform
+
 from submit.models import Submit
 
 
@@ -7,5 +9,7 @@ class SubmitAdmin(admin.ModelAdmin):
     list_display = ['user', 'task', 'message', 'language', 'timestamp']
     search_fields = ['user__username', 'task__id', 'task__task_set__id']
     list_filter = ['language', 'message']
+
+    form = select2_modelform(Submit)
 
 admin.site.register(Submit, SubmitAdmin)
